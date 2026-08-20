634919e189fa56c9cf03bad3cc703596ec4de69f
kalpdoshi <kalp.doshi@students.iiit.ac.in>
2026-08-20 19:33:40 +0530

    loop reading with EOF handled

--- c-shell/src/main.c
+++ c-shell/src/main.c
@@ -1,1 +1,15 @@
-int main(void) { return 0; }
+#include<stdio.h>
+#include<string.h>
+#define BUFFER_SIZE 1024
+int main() {
+    while(1) {
+        printf("<shell>:");
+        fflush(stdout);
+        char command[BUFFER_SIZE];
+        if(fgets(command, BUFFER_SIZE, stdin) == NULL) {
+            break;
+        }
+        command[strcspn(command, "\n")] = 0; 
+
+    }
+}
